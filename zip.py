9fee2e618ae6263ce0cb79f555bb8bb6d4a87498
PatrichBravo <patrich.bravo@gmail.com>
2023-10-01 13:38:24 +1300

    First commit

---
+++ zip.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Select file to compress:")
+input1 = sg.Input()
+choose_buttom1 = sg.FileBrowse("Choose")
+
+label2 = sg.Text("Select file to compress:")
+input2 = sg.Input()
+choose_buttom2 = sg.FileBrowse("Choose")
+
+
+
